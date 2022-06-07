5c1ff1c786c5fc958e587feb9056cc7eb19d893b
ikyrie <mah.c5r@gmail.com>
2022-06-07 16:50:46 -0300

    Projeto inicial

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:coresdinamicas/screens/tela_inicial.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  bool opacidade = true;
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: InitialScreen(),
+    );
+  }
+}
+
+
+
